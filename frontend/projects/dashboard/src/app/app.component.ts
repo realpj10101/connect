331c44a4df63d7608df8bd5f90a855bfe64fa3d5
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-08 17:59:06 +0330

    modify app component

--- frontend/projects/dashboard/src/app/app.component.ts
+++ frontend/projects/dashboard/src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./components/account/register/register.component";
+import { LoginComponent } from "./components/account/login/login.component";
+import { AccountService } from './services/account.service';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'dashboard';
+  private _accountService = inject(AccountService);
+  router = inject(Router);
+
+  constructor() {
+    this.initUserOnPageRefresh();
+  }
+
+  initUserOnPageRefresh(): void {
+    const loggedInPlayerStr = localStorage.getItem('loggedInUser');
+
+    if (loggedInPlayerStr) {
+      // First, check if user's token is not expired.
+      this._accountService.authorizeLoggedInUser();
+
+      // Then, set the authorized logged-in user
+      this._accountService.setCurrentUser(JSON.parse(loggedInPlayerStr))
+    }
+  }
 }
