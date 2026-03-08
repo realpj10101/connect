ddc40522157825a2522fca078e7c965a294c1770
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-08 18:15:18 +0330

    modify landing app component

--- frontend/projects/landing/src/app/app.component.ts
+++ frontend/projects/landing/src/app/app.component.ts
@@ -1,26 +1,31 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ThemeService } from '../../../ui/src/lib/services/theme.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../../ui/src/public-api';
+import { ChatMessage } from '../models/chat-message.model';
+import { FeatureCard } from '../models/feature-card.model';
+import { RegisterComponent } from "../../../dashboard/src/app/components/account/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterLink, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-  themeService = inject(ThemeService);
-
-  selectedTheme = '';
-
-  ngOnInit(): void {
-    const theme = this.themeService.getSelectedTheme();
-    this.selectedTheme = theme.name;
-  }
-
-  changeTheme(themeName: string) {
-    this.themeService.setSelectedTheme(themeName);
-    this.selectedTheme = themeName;
-  }
+export class AppComponent {
 }
+
+
+// toggleMenu() {
+//   this.menuOpen = !this.menuOpen;
+// }
+
+// ngOnInit(): void {
+//   const theme = this.themeService.getSelectedTheme();
+//   this.selectedTheme = theme.name;
+// }
+
+// changeTheme(themeName: string) {
+//   this.themeService.setSelectedTheme(themeName);
+//   this.selectedTheme = themeName;
+// }
