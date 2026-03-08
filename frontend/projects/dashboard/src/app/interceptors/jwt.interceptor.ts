918138627e4cea97c4fda5a3ea0658c6257d3b55
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-08 18:06:12 +0330

    create jwt and error interceptor

---
+++ frontend/projects/dashboard/src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoggedInUser } from '../models/logged-in-user.model';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  const loggedInUserStr: string | null = localStorage.getItem('loggedInUser');
+
+  if (loggedInUserStr) {
+    const loggedInUser: LoggedInUser = JSON.parse(loggedInUserStr);
+
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${loggedInUser.token}`
+      }
+    })
+  }
+
+  return next(req);
+};
