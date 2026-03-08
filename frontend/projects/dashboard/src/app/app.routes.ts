fa1540505491685a9ed9554fb88bc6590c259e6b
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-08 18:04:21 +0330

    create components routes and use guards

--- frontend/projects/dashboard/src/app/app.routes.ts
+++ frontend/projects/dashboard/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/account/login/login.component';
+import { RegisterComponent } from './components/account/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from './guards/auth.guard';
+import { ProfileComponent } from './components/profile/profile.component';
+import { CreateRoomComponent } from './components/rooms/create-room/create-room.component';
+import { authLoggedInGuard } from './guards/auth-logged-in.guard';
+import { RoomViewComponent } from './components/rooms/room-view/room-view.component';
+import { RoomDetailsComponent } from './components/rooms/room-details/room-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        canActivateChild: [],
+        children: [
+            { path: '', component: DashboardComponent },
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'profile', component: ProfileComponent },
+            { path: 'create-room', component: CreateRoomComponent },
+            { path: 'room-view/:id', component: RoomViewComponent},
+            { path: 'room-details/:id', component: RoomDetailsComponent},
+            { path: 'create-room', component: CreateRoomComponent},
+            { path:'profile', component: ProfileComponent}
+        ]
+    },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authLoggedInGuard],
+        canActivateChild: [],
+        children: [
+            { path: 'sign-in', component: LoginComponent },
+            { path: 'sign-up', component: RegisterComponent },
+        ]
+    },
+];
