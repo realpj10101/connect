753a77c196d07b798459a58388f770d936424930
realpj10101 <parsajfr1385oo@gmail.com>
2026-03-12 19:14:14 +0330

    create chat item model

---
+++ frontend/projects/dashboard/src/app/models/chat-item.model.ts
@@ -0,0 +1,15 @@
+export interface ChatItem {
+    id: string;
+    type: string;
+    senderUserName: string;
+    message: string | null;
+    duration: number | null;
+    fileSize: number | null;
+    createdAt: Date;
+    isPlaying: boolean;
+    audioUrl?: string;
+    audioRef?: HTMLAudioElement;
+    isDownloading?: boolean;
+    downloadProgress?: number;
+    isDownloaded?: boolean;
+}
